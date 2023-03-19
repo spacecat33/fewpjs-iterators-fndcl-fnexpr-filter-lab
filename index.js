f170d6100d0b946c207da145f3c8493325eedf5e
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-19 17:38:50 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(s => s.toLowerCase() === string.toLowerCase()
+  )
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(s => s.toLowerCase().indexOf(string.toLowerCase()) === 0
+  )
+}
+
+function matchName(array, string) {
+  return array.filter(s => s.name === string)
+}
+
+
+// OR less dry:
+//  
+// function findMatching(drivers, name) {
+//     return drivers.filter(driver => {
+//         if(driver.toLowerCase() === name.toLowerCase()) {
+//             return driver;
+//         }
+//     });
+// }
+
+// function fuzzyMatch(drivers, letters) {
+//     return drivers.filter(driver => driver.startsWith(letters));
+// }
+
+// function matchName(drivers, name) {
+//     return drivers.filter(driver => {
+//         if(driver.name === name) {
+//             return driver.name;
+//         }
+//     })
+// }
